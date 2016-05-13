427d302660e874a4c2e8a01b2aa8a479ec631e12
aniruddha <aniruddha.kudalkar@gmail.com>
2016-05-13 09:03:43 +0530

    added relative layout to activity layout

--- app/src/main/java/com/codekul/layouts/MainActivity.java
+++ app/src/main/java/com/codekul/layouts/MainActivity.java
@@ -1,13 +1,31 @@
 package com.codekul.layouts;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
+    private Button btnCenter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Click click = new Click();
+        btnCenter = (Button) findViewById(R.id.btnCenter);
+        btnCenter.setOnClickListener(click);
+    }
+
+    private class Click implements View.OnClickListener{
+
+        @Override
+        public void onClick(View v) {
+            if(v.getId() == btnCenter.getId()){
+
+            }
+        }
     }
 }
